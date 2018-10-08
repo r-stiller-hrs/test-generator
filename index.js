82e3db120ceb022ec8660d6a09c445208adc45ad
rstiller-hrs <robert.stiller@innovation.hrs.com>
2018-10-08 08:27:24 +0200

    initial commit for the test generator

---
+++ index.js
@@ -0,0 +1,34 @@
+class ArgumentError extends Error {}
+
+function transformHotel(hotel) {
+    if (!hotel.name) {
+        throw new ArgumentError('hotel name must not be empty');
+    } else if (hotel.name.length < 5) {
+        throw new ArgumentError('hotel name too short');
+    } else if (hotel.name.length > 64) {
+        throw new ArgumentError('hotel name too large');
+    }
+
+    var newHotel = {
+        primaryName: hotel.name,
+        description: hotel.description,
+        rooms: hotel.rooms,
+        features: []
+    };
+
+    if (hotel.features) {
+        Object.keys(hotel.features).forEach(feature => {
+            newHotel.features.push({
+                name: feature,
+                value: `${hotel.features[feature]}`
+            });
+        })
+    }
+
+    return newHotel;
+}
+
+module.exports = {
+    ArgumentError,
+    transformHotel,
+};
---
+++ test/debug.js
@@ -0,0 +1,17 @@
+function printAllValues(generator) {
+    for (let value of generator) {
+        console.log(value);
+    }
+}
+
+function printFirstValue(generator) {
+    for (let value of generator) {
+        console.log(value);
+        break;
+    }
+}
+
+module.exports = {
+    printAllValues,
+    printFirstValue,
+};
